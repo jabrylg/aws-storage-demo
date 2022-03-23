e07441578fd3e92ea17ee1d9f07b63b0cd5639d1
Jabryl Guy <jabrylg@gmail.com>
2022-03-23 15:02:52 -0400

    amplify dependancies added

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "awsstoragedemo": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import Amplify, { Storage } from 'aws-amplify/core';
 
 function App() {
+  useEffect(() => {
+      Amplify.configure({
+        Auth: {
+            identityPoolId: 'us-east-1:6909dba9-ccd7-444c-bbe5-5afc85e29232',
+            region: 'us-east-1'
+        },
+        Storage: {
+          AWSS3: {
+
+          }
+        }
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> AWS Storage Demo</h1>
       </header>
     </div>
   );
